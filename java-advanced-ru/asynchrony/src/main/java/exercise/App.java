588c29cebcbb30488e17a3fb8fa8e3bd5259f449
@hexlet/cli <support@hexlet.io>
2024-05-13 00:18:08 +0300

    submit java-advanced-ru/asynchrony

---
+++ java-advanced-ru/asynchrony/src/main/java/exercise/App.java
@@ -0,0 +1,98 @@
+package exercise;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.Arrays;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.Files;
+import java.io.File;
+import java.nio.file.StandardOpenOption;
+
+class App {
+
+    // BEGIN
+    private static Path getFullPath(String filePath) {
+        return Paths.get(filePath).toAbsolutePath().normalize();
+    }
+
+    public static CompletableFuture<String> unionFiles(String source1, String source2, String dest) {
+
+        CompletableFuture<String> content1 = CompletableFuture.supplyAsync(() -> {
+            String content = "";
+
+            try {
+                content = Files.readString(getFullPath(source1));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            return content;
+        });
+
+        CompletableFuture<String> content2 = CompletableFuture.supplyAsync(() -> {
+
+            String content = "";
+            try {
+                content = Files.readString(getFullPath(source2));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            return content;
+        });
+
+        return content1.thenCombine(content2, (cont1, cont2) -> {
+            String union = cont1 + cont2;
+            try {
+                Files.writeString(getFullPath(dest), union, StandardOpenOption.CREATE);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            return "ok!";
+
+        }).exceptionally(ex -> {
+            System.out.println("Exception - " + ex.getMessage());
+            return "Unknown!";
+        });
+    }
+
+    public static CompletableFuture<Long> getDirectorySize(String path) {
+
+        return CompletableFuture.supplyAsync(() -> {
+            Long size;
+            try {
+                size = Files.walk(getFullPath(path), 1)
+                        .filter(Files::isRegularFile)
+                        .mapToLong(p -> {
+                            try {
+                                return Files.size(p);
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        })
+                        .sum();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            return size;
+
+        }).exceptionally(ex -> {
+            System.out.println("Exception - " + ex.getMessage());
+            return null;
+        });
+    }
+    // END
+
+    public static void main(String[] args) throws Exception {
+        // BEGIN
+        CompletableFuture<String> result = unionFiles(
+                "src/main/resources/file1.txt",
+                "src/main/resources/file2.txt",
+                "src/main/resources/dest.txt"
+        );
+        CompletableFuture<Long> size = getDirectorySize("src/main/resources");
+        result.get();
+        System.out.println("done!");
+        System.out.println(size.get());
+        // END
+    }
+}
+
